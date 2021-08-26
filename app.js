b0ed88e0c4e36d522b443ceb061d64f743cd6f4c
Vamsi <kothapallivamsikrishna@gmail.com>
2021-08-25 23:06:42 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const notifier = require('mail-watcher');
+const twilio_sms_client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const imap = {
+  user: process.env.USERNAME,
+  password: process.env.PASSWORD,
+  host: process.env.IMAP_HOST,
+  port: process.env.IMAP_PORT, // imap port
+  tls: true,// use secure connection
+  tlsOptions: { rejectUnauthorized: false }
+};
+ 
+notifier(imap)
+  .on('mail', mail => {
+      const subjectRegex = new RegExp(process.env.MAIL_SUBJECT_REGEX)
+      const bodyRegex = new RegExp(process.env.MAIL_BODY_REGEX)
+      if(mail.subject.match(subjectRegex)){
+          sendSms(`Check your email for subject match subject: ${mail.subject}`)
+      }else if(mail.body.match(bodyRegex)){
+          sendSms(`Check your email for body match subject: ${mail.subject}`)
+      }
+  })
+  .on('error',err => console.log(err))
+  .start();
+
+  function sendSms(body){
+    twilio_sms_client.messages
+    .create({
+       body: body,
+       from: process.env.SMS_SEND_MOBILE,
+       to: process.env.SMS_RECEIVE_MOBILE
+     })
+    .then(message => {
+        console.log(`SMS Notified for email subject:${mail.subject}, SMS_ID:${message.sid}`)
+    });
+  }
